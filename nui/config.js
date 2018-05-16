f54803426133cb3691011b6364830908d116d3b4
Emre Cürgül <pongo1999712@gmail.com>
2018-05-16 18:13:08 +0200

    v2.1

--- nui/config.js
+++ nui/config.js
@@ -11,8 +11,6 @@ function handleConfig(config) {
 	setTitleTextFromConfig(config);
 	setTitleColorFromConfig(config);
 	setSubtitleColorFromConfig(config);
-	//setItemsColorFromConfig(config);
-	//setSelectedItemsColorFromConfig(config);
 	
 	if (config.items.pagelimit)
 		pagelimit = config.items.pagelimit;
@@ -58,31 +56,3 @@ function setSubtitleColorFromConfig(config) {
 		$subtitle.css("color", "rgb(" + color.font.r + "," + color.font.g + "," + color.font.b + ")");
 	}
 }
-
-/*function setItemsColorFromConfig(config) {
-	var color = config.items.color;
-	if (color) {
-		if (color.css)
-			$("head").append(".menuoption{background:'" + color.css + "'}");
-		else {
-			var from = color.from;
-			var to = color.to;
-			$("head").append("<style id='addedCSSa' type='text/css'>.menuoption{background:'linear-gradient(90deg, rgba(" +
-				from.r + "," + from.g + "," + from.b + "," + from.a + "), rgba(" + to.r + "," + to.g + "," + to.b + "," + to.a + ")'}</style>");
-		}
-	}
-}
-
-function setSelectedItemsColorFromConfig(config) {
-	var color = config.items.color.selected;
-	if (color) {
-		if (color.css)
-			$("head").append(".menuoption.selected{background:'" + color.css + "'}");
-		else {
-			var from = color.from;
-			var to = color.to;
-			$("head").append("<style id='addedCSSb' type='text/css'>.menuoption.selected{background:'linear-gradient(90deg, rgba(" +
-				from.r + "," + from.g + "," + from.b + "," + from.a + "), rgba(" + to.r + "," + to.g + "," + to.b + "," + to.a + ")'}</style>");
-		}
-	}
-}*/
