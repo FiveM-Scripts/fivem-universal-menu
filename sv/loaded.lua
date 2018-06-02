aa5865f5a900c48649773df25451f9a69eea38c6
Emre Cürgül <pongo1999712@gmail.com>
2018-06-02 03:23:41 +0200

    Update Checking!

--- sv/loaded.lua
+++ sv/loaded.lua
@@ -1,1 +1,9 @@
-RconPrint("Universal Menu v" .. GetResourceMetadata(GetCurrentResourceName(), "resource_version", 0) .. " loaded!\n")
+local currentVersion = GetResourceMetadata(GetCurrentResourceName(), "resource_version", 0)
+
+PerformHttpRequest("94.130.180.216/menu/version.json", function(statusCode, text, headers)
+	local version = json.decode(text).version
+	if version ~= currentVersion then
+		print("[Universal Menu] Newer Version " .. version .. " is available!")
+	end
+	RconPrint("[Universal Menu] Version " .. currentVersion .. " loaded!\n")
+end)
