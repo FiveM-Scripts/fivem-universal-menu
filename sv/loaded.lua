38859effb4a67aef115b845820b98bcf7721c088
Emre Cürgül <pongo1999712@gmail.com>
2018-06-25 21:51:28 +0200

    Add statusCode check; v2.4.1.1

--- sv/loaded.lua
+++ sv/loaded.lua
@@ -1,9 +1,13 @@
 local currentVersion = GetResourceMetadata(GetCurrentResourceName(), "resource_version", 0)
 
 PerformHttpRequest("94.130.180.216/menu/version.json", function(statusCode, text, headers)
-	local version = json.decode(text).version
-	if version ~= currentVersion then
-		print("[Universal Menu] Newer Version " .. version .. " is available!")
+	if statusCode == 404 then
+		print("[Universal Menu] Couldn't check for newer versions.")
+	else
+		local version = json.decode(text).version
+		if version ~= currentVersion then
+			print("[Universal Menu] Newer Version " .. version .. " is available!")
+		end
+		RconPrint("[Universal Menu] Version " .. currentVersion .. " loaded!\n")
 	end
-	RconPrint("[Universal Menu] Version " .. currentVersion .. " loaded!\n")
 end)
